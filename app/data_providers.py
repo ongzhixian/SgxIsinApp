f461df2daae6deda222f1582a60ed7eb897ef45f
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-09-07 18:01:17 +0800

    update

--- app/data_providers.py
+++ app/data_providers.py
@@ -10,12 +10,35 @@ import mysql.connector
 class MySqlDataProvider(object):
     """Data provider for MySql"""
 
-    def __init__(self, database_setting_name):
-        pass
-        # prefix = ''
-        # if 'PYTHONANYWHERE_DOMAIN' not in environ:
-        #     prefix = 'dev_'
-        # setting_name = f'{prefix}{database_setting_name}'
-        # self.database_setting_name = database_setting_name
-        # self.db_settings = app_secrets['MYSQL'][setting_name]
-        # self.database_name = self.db_settings['DATABASE']
+    def __init__(self, database_settings):
+        self.database_settings = database_settings
+        print(self.database_settings)
+
+    def get_server_connection(self):
+        mysql_connection = mysql.connector.connect(
+            host=self.database_settings['HOST'],
+            port=self.database_settings['PORT'],
+            user=self.database_settings['USERNAME'],
+            password=self.database_settings['PASSWORD'],
+            collation='utf8mb4_unicode_ci',
+            charset='utf8mb4'
+        )
+        return mysql_connection
+
+    def execute_batch(self, sql, data_rows):
+        connection = None
+        mycursor = None
+        try:
+            connection = self.get_server_connection()
+            mycursor = connection.cursor()
+            mycursor.executemany(sql, data_rows)
+            connection.commit()
+            return (0, None) if mycursor is None else (mycursor.rowcount, None)
+        except Exception as ex:
+            logging.error(ex)
+            return (-1, ex)
+        finally:
+            if mycursor is not None:
+                mycursor.close()
+            if connection is not None:
+                connection.close()
